f938e03dd00a70759ef473ddfbfb5e0cf2f6be91
Jun Tian <find_my_way@foxmail.com>
2020-11-01 15:02:10 +0800

    Update 124.binary-tree-maximum-path-sum.jl

--- test/problems/124.binary-tree-maximum-path-sum.jl
+++ test/problems/124.binary-tree-maximum-path-sum.jl
@@ -1,5 +1,15 @@
-@testset "124.binary-tree-maximum-path-sum.jl" begin
-    t = TreeNode(-10, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+#! format: off
 
+@testset "124.binary-tree-maximum-path-sum.jl" begin
+    t = TreeNode(
+        -10,	
+        TreeNode(9),	
+        TreeNode(	
+            20,	
+            TreeNode(15),	
+            TreeNode(7)	
+        )	
+    )
+    
     @test max_path_sum(t) == 42
 end
