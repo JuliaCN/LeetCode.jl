58ce31905e3361db803db1cd690ec2aefc6b060c
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2020-11-28 09:12:02 +0800

    Import DataStructures.jl

--- src/problems/454.4-sum-ii.jl
+++ src/problems/454.4-sum-ii.jl
@@ -24,11 +24,7 @@ The two tuples are:
 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
 """
-using DataStructures
-# @lc code=start
 function four_sum_count(A::Vector{Int}, B::Vector{Int}, C::Vector{Int}, D::Vector{Int})::Int
     dic = counter([a + b for a in A for b in B])
     return sum(get(dic, -c - d, 0) for c in C for d in D)
 end
-# @lc code=end
-
