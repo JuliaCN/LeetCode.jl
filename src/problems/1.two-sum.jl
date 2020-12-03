052b01862ce42b3fa48fbf9c7c745fb26d79d2f7
Johnny Chen <johnnychen94@hotmail.com>
2020-12-04 00:39:53 +0800

    WIP: correctly format problems

--- src/problems/1.two-sum.jl
+++ src/problems/1.two-sum.jl
@@ -1,33 +1,34 @@
-"""
-@lc app=leetcode id=1 lang=python
+# ---
+# title: 1. Two Sum
+# id: id1
+# author: Jun Tian
+# date: 2020-10-31
+# app: leetcode
+# lang: julia
+# ---
 
-[1] Two Sum
+# https://leetcode.com/problems/two-sum/description/
 
-https://leetcode.com/problems/two-sum/description/
+# - algorithms
+# - Easy (44.95%)
+# - Total Accepted:    2.5M
+# - Total Submissions: 5.5M
+# - Testcase Example:  `[2,7,11,15]`
 
-algorithms
-Easy (44.95%)
-Total Accepted:    2.5M
-Total Submissions: 5.5M
-Testcase Example:  '[2,7,11,15]\n9'
+# Given an array of integers, return **indices** of the two
+# numbers such that they add up to a specific target.
 
-<p>Given an array of integers, return <strong>indices</strong> of the two
-numbers such that they add up to a specific target.</p>
+# You may assume that each input would have
+# **_exactly_** one solution, and you may not use the
+# same element twice.
 
-<p>You may assume that each input would have
-<strong><em>exactly</em></strong> one solution, and you may not use the
-<em>same</em> element twice.</p>
+# Example:
 
-<p><strong>Example:</strong></p>
+# Given `nums = [2, 7, 11, 15]`, `target = 9`.
+# Because `nums[0] + nums[1] = 2 + 7 = 9`,
+# return `[0, 1]`.
 
-<pre>
-Given nums = [2, 7, 11, 15], target = 9,
 
-Because nums[<strong>0</strong>] + nums[<strong>1</strong>] = 2 + 7 = 9,
-return [<strong>0</strong>, <strong>1</strong>].
-</pre>
-
-"""
 function two_sum(nums::Vector{Int}, target::Int)::Union{Nothing,Tuple{Int,Int}}
     seen = Dict{Int,Int}()
     for (i, n) in enumerate(nums)
