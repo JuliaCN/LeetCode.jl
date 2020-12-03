ed843a66b084c879e0f810b35228d2cf0053e373
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2020-12-03 14:26:59 +0800

    Add Problem 204

--- test/problems/204.count-primes.jl
+++ test/problems/204.count-primes.jl
@@ -1,4 +1,6 @@
 @testset "204.count-primes.jl" begin
-    @test search_matrix(arr, 5) == true
-    @test search_matrix(arr, 20) == false
+    @test count_primes(10) == count_primes(11) == 4
+    @test count_primes(1000) == 168
+    @test count_primes(10000) == 1229
+    @test count_primes(100000) == 9592
 end
