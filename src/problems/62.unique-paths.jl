aa16338861becfaa60f891b834d519354fcccd70
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2020-12-09 07:43:48 +0800

    Use the new templete

--- src/problems/62.unique-paths.jl
+++ src/problems/62.unique-paths.jl
@@ -1,48 +1,52 @@
-#
-# @lc app=leetcode id=62 lang=python3
-#
-# [62] Unique Paths
-#
+# ---
+# title: 62. Unique Paths
+# id: problem62
+# author: AquaIndigo
+# date: 2020-12-09
+# difficulty: Medium
+# categories: array | dynamic-programming
+# link: <https://leetcode.com/problems/unique-paths/description/>
+# hidden: true
 
-"""
-A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
 
-The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+# A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
 
-How many possible unique paths are there?
+# The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+# How many possible unique paths are there?
 
  
 
-Example 1:
+# Example 1:
 
 
-Input: m = 3, n = 7
-Output: 28
-Example 2:
+# Input: m = 3, n = 7
+# Output: 28
+# Example 2:
 
-Input: m = 3, n = 2
-Output: 3
-Explanation:
-From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
-1. Right -> Down -> Down
-2. Down -> Down -> Right
-3. Down -> Right -> Down
-Example 3:
+# Input: m = 3, n = 2
+# Output: 3
+# Explanation:
+# From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+# 1. Right -> Down -> Down
+# 2. Down -> Down -> Right
+# 3. Down -> Right -> Down
+# Example 3:
 
-Input: m = 7, n = 3
-Output: 28
-Example 4:
+# Input: m = 7, n = 3
+# Output: 28
+# Example 4:
 
-Input: m = 3, n = 3
-Output: 6
+# Input: m = 3, n = 3
+# Output: 6
  
 
-Constraints:
+# Constraints:
 
-1 <= m, n <= 100
-It's guaranteed that the answer will be less than or equal to 2 * 109.
-"""
-# @lc code=start
+# 1 <= m, n <= 100
+# It's guaranteed that the answer will be less than or equal to 2 * 109.
+
+## @lc code=start
 unique_paths(m::Int, n::Int) = binomial(m + n - 2, n - 1)
 
-# @lc code=end
+## @lc code=end
