784238e651f9da44edde453f17566a40d99710c2
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2020-12-17 09:19:14 +0800

    Add Problem 714

--- src/problems/714.best-time-to-buy-and-sell-stock-with-transaction-fee.jl
+++ src/problems/714.best-time-to-buy-and-sell-stock-with-transaction-fee.jl
@@ -43,6 +43,11 @@
 # 
 ## @lc code=start
 using LeetCode
-
-## add your code here:
+function max_profit(price::Vector{Int}, fee::Int)::Int
+    dp1, dp2 = 0, -50000
+    for p in price
+        dp1, dp2 = max(dp1, dp2 + p - fee), max(dp2, dp1 - p)
+    end
+    return dp1
+end
 ## @lc code=end
---
+++ test/problems/714.best-time-to-buy-and-sell-stock-with-transaction-fee.jl
@@ -0,0 +1,3 @@
+@testset "714.best-time-to-buy-and-sell-stock-with-transaction-fee.jl" begin
+    @test max_profit([1, 3, 2, 8, 4, 9], 2) == 8
+end
