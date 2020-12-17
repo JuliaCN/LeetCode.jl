374672df19f40c6a01cb2c639954de48780c0590
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2020-12-17 12:38:47 +0800

    Solve the conflicts between functions with the same name and params

--- src/problems/714.best-time-to-buy-and-sell-stock-with-transaction-fee.jl
+++ src/problems/714.best-time-to-buy-and-sell-stock-with-transaction-fee.jl
@@ -43,7 +43,7 @@
 # 
 ## @lc code=start
 using LeetCode
-function max_profit(price::Vector{Int}, fee::Int)::Int
+function max_profit714(price::Vector{Int}, fee::Int)::Int
     dp1, dp2 = 0, -50000
     for p in price
         dp1, dp2 = max(dp1, dp2 + p - fee), max(dp2, dp1 - p)
