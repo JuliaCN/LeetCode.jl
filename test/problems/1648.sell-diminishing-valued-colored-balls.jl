00ecb223dae9e3226852c8ed141c43588a994bd6
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2020-12-17 12:39:13 +0800

    Rename the test name

--- test/problems/1648.sell-diminishing-valued-colored-balls.jl
+++ test/problems/1648.sell-diminishing-valued-colored-balls.jl
@@ -1,4 +1,4 @@
-@testset "5563.sell-diminishing-valued-colored-balls.jl" begin
+@testset "1648.sell-diminishing-valued-colored-balls.jl" begin
     @test max_profit([497978859, 167261111, 483575207, 591815159], 836556809) == 373219333
     @test max_profit([2, 5], 4) == 14
     @test max_profit([3, 5], 6) == 19
