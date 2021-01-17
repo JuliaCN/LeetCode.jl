8f9a2b79783e0f17c7be0ef6ac64a63e92dad2af
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2021-01-17 17:43:32 +0800

    Add Problem 149

--- src/problems/149.max-points-on-a-line.jl
+++ src/problems/149.max-points-on-a-line.jl
@@ -51,6 +51,18 @@
 # 
 ## @lc code=start
 using LeetCode
-
-## add your code here:
+function max_points(points::Vector{Vector{Int}})
+    res = 0
+    for i in 1:length(points)
+        if res > length(points) - i 
+            return res
+        end
+        cnt = DefaultDict{Rational, Int}(1)
+        for j in i+1:length(points)
+            cnt[(points[i][2] - points[j][2]) // (points[i][1] - points[j][1])] += 1
+        end
+        res = max(res, maximum(p.second for p in cnt))
+    end    
+    res
+end
 ## @lc code=end
