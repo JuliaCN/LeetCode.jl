e770614910dd04c54e63e4378d2ea9a2bd1f55db
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2021-01-17 16:45:31 +0800

    Add Problem 149

---
+++ test/problems/149.max-points-on-a-line.jl
@@ -0,0 +1,5 @@
+@testset "149.max-points-on-a-line.jl" begin
+    @test max_points([[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]) == 4
+    @test max_points([[1,1],[2,2],[3,3]]) == 3
+    @test max_points([[0,0],[94911150,94911151],[94911151,94911152]]) == 2
+end
