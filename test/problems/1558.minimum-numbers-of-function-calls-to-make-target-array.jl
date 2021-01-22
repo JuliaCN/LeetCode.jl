2911997ea811a2f8e51b5e18d3c8b314b7ab0427
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2021-01-22 16:30:39 +0800

    add the test name

--- test/problems/1558.minimum-numbers-of-function-calls-to-make-target-array.jl
+++ test/problems/1558.minimum-numbers-of-function-calls-to-make-target-array.jl
@@ -1,4 +1,4 @@
-@testset "" begin
+@testset "1558.minimum-numbers-of-function-calls-to-make-target-array.jl" begin
     @test min_operations1558([1, 5]) == 5
     @test min_operations1558([2,2]) == 3
     @test min_operations1558([4,2,5]) == 6
