d56cc62dbe326bfb060b45890d1dcc4d517bf8db
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2021-01-27 10:52:51 +0800

    Add Problem 1577

--- src/problems/1577.number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.jl
+++ src/problems/1577.number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 1577. Number of Ways Where Square of Number Is Equal to Product of Two Numbers
 # id: problem1577
-# author: Tian Jun
-# date: 2020-10-31
+# author: Indigo
+# date: 2021-01-27
 # difficulty: Medium
 # categories: Hash Table, Math
 # link: <https://leetcode.com/problems/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/description/>
@@ -68,5 +68,32 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function num_triplets(nums1::Vector{Int}, nums2::Vector{Int})
+    cnt1 = counter(nums1)
+    cnt2 = counter(nums2)
+    sqr_cnt1 = Dict(k ^ 2 => v for (k, v) in cnt1)
+    sqr_cnt2 = Dict(k ^ 2 => v for (k, v) in cnt2)
+    res = 0
+    for (k1, v1) in cnt1
+        for (k2, v2) in sqr_cnt2
+            if k1 ^ 2 == k2
+                res += (v1 - 1) * v1 * v2
+            else
+                res += cnt1[k2 ÷ k1] * v1 * v2     
+            end
+        end
+    end
+    for (k1, v1) in cnt2
+        for (k2, v2) in sqr_cnt1
+            if k2 % k1 == 0
+                if k1 ^ 2 == k2
+                    res += (v1 - 1) * v1 * v2
+                else
+                    res += cnt2[k2 ÷ k1] * v1 * v2
+                end
+            end
+        end
+    end
+    res ÷ 2
+end
 ## @lc code=end
