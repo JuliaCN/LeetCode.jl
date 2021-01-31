eaedbc69a3ac9a54c40507ca7ae061e5b2508241
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2021-01-31 14:05:41 +0800

    Add Problem 1007

--- src/problems/1007.minimum-domino-rotations-for-equal-row.jl
+++ src/problems/1007.minimum-domino-rotations-for-equal-row.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 1007. Minimum Domino Rotations For Equal Row
 # id: problem1007
-# author: Tian Jun
-# date: 2020-10-31
+# author: Indigo
+# date: 2021-01-30
 # difficulty: Medium
 # categories: Array, Greedy
 # link: <https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/description/>
@@ -56,5 +56,21 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function min_domino_rotations(A::Vector{Int}, B::Vector{Int})
+    base1, base2 = A[1], B[1]
+    cnt1, cnt2 = 0, 0
+    for i = 1:length(A)
+        !(base1 in (A[i], B[i])) && (base1 = 0)
+        !(base2 in (A[i], B[i])) && (base2 = 0)
+        if base1 == base2 == 0
+            return -1
+        end
+        (base1 != 0 && A[i] != base1) && (cnt1 += 1)
+        (base2 != 0 && A[i] != base2) && (cnt2 += 1)
+    end
+    res = length(A)
+    (base1 != 0) && (res = min(base1, length(A) - base1))
+    (base2 != 0) && (res = min(res, base2, length(A) - base2))
+    res
+end
 ## @lc code=end
