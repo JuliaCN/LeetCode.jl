fa324cdf8602a4fae1c04faef0471bb7a5b8b048
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2021-01-31 15:07:26 +0800

    Add Problem 1008

--- test/problems/1008.construct-binary-search-tree-from-preorder-traversal.jl
+++ test/problems/1008.construct-binary-search-tree-from-preorder-traversal.jl
@@ -1,4 +1,4 @@
-@test "1008.construct-binary-search-tree-from-preorder-traversal.jl" begin
+@testset "1008.construct-binary-search-tree-from-preorder-traversal.jl" begin
     @test bst_from_preorder([8, 5, 1, 7, 10, 12]) == TreeNode(
         8,
         TreeNode(5, TreeNode(1), TreeNode(7)),
