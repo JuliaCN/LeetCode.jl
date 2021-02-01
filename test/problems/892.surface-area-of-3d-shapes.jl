f095b18a5715041db12004a84838637a8c54224b
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2021-02-01 16:02:09 +0800

    Add Problem 892

---
+++ test/problems/892.surface-area-of-3d-shapes.jl
@@ -0,0 +1,7 @@
+@testset "892.surface-area-of-3d-shapes.jl" begin
+    @test surface_area([[1,2],[3,4]]) == 34
+    @test surface_area([[1,0],[0,2]]) == 16
+    @test surface_area([[1,1,1],[1,0,1],[1,1,1]]) == 32
+    @test surface_area([[2,2,2],[2,1,2],[2,2,2]]) == 46
+    @test surface_area([[2]]) == 10
+end
