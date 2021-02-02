82d209a7462df7368bb9729fd7e3198b548c4f15
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2021-02-02 17:19:51 +0800

    Add Problem 453

---
+++ test/problems/451.sort-characters-by-frequency.jl
@@ -0,0 +1,5 @@
+@testset "451.sort-characters-by-frequency.jl" begin
+    @test frequency_sort("tree") == "eetr"
+    @test frequency_sort("cccaaa") == "cccaaa"
+    @test frequency_sort("Aabb") == "bbAa"
+end
