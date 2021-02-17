8b194587004391442693724d6247b606cd15c462
Qling <rfhklwt@163.com>
2021-02-17 17:03:29 +0800

    Add Problem 566

--- src/problems/566.reshape-the-matrix.jl
+++ src/problems/566.reshape-the-matrix.jl
@@ -62,5 +62,20 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function matrixReshape(nums::Vector{Vector{Int}}, r::Int, c::Int)
+    m, n = length(nums), length(nums[1])
+
+    if m * n != r * c
+        return nums
+    end
+
+    ans = [[0 for _ in 1: c] for _ in 1: r]
+
+    for x in 0: m * n - 1
+        ans[x ÷ c + 1][(x % c) + 1] = nums[x ÷ n + 1][(x % n) + 1]
+    end
+
+    return ans
+end
+
 ## @lc code=end
