432bb8a39e2d64aebbfc09c30c0eb25005f34d02
sjtu_suyao <j.c.f.gauss@sjtu.edu.cn>
2021-02-18 14:33:58 +0800

    Add Problem 994

--- src/problems/994.rotting-oranges.jl
+++ src/problems/994.rotting-oranges.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 994. Rotting Oranges
 # id: problem994
-# author: Tian Jun
-# date: 2020-10-31
+# author: Indigo
+# date: 2021-02-18
 # difficulty: Medium
 # categories: Breadth-first Search
 # link: <https://leetcode.com/problems/rotting-oranges/description/>
@@ -61,7 +61,36 @@
 # 
 # 
 ## @lc code=start
-using LeetCode
+using LeetCode, DataStructures
 
-## add your code here:
+function oranges_rotting(grid::Vector{Vector{Int}})
+    nr, nc = length(grid), length(grid[1])
+    visited = fill(false, nr, nc)
+    q = Queue{Tuple{Int,Int,Int}}()
+    left, res = 0, 0
+    for r in 1:nr, c in 1:nc
+        if grid[r][c] == 2
+            enqueue!(q, (r, c, 0))
+        elseif grid[r][c] == 1
+            left += 1
+        end
+    end
+    dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    while !isempty(q)
+        tp = dequeue!(q)
+        for dir in dirs
+            nb = (tp[1] + dir[1], tp[2] + dir[2], tp[3] + 1)
+            if 0 < nb[1] ≤ nr &&
+               0 < nb[2] ≤ nc &&
+               !visited[nb[1], nb[2]] &&
+               grid[nb[1]][nb[2]] == 1
+                left -= 1
+                visited[nb[1], nb[2]] = true
+                enqueue!(q, nb)
+            end
+        end
+        res = tp[3]
+    end
+    left == 0 ? res : -1
+end
 ## @lc code=end
