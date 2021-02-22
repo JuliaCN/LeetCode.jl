55600ec64928e6df2de6fe73117226934c84664a
Qling <rfhklwt@163.com>
2021-02-22 12:32:47 +0800

    Formatting Problem 766

--- src/problems/766.toeplitz-matrix.jl
+++ src/problems/766.toeplitz-matrix.jl
@@ -66,12 +66,11 @@ using LeetCode
 function is_toeplitz_matrix(matrix::Vector{Vector{Int}})
     m, n = length(matrix), length(matrix[1])
 
-    for i in 1: m - 1
-        (matrix[i][1: end - 1] != matrix[i + 1][2: end]) && return false
+    for i in 1:(m - 1)
+        (matrix[i][1:(end - 1)] != matrix[i + 1][2:end]) && return false
     end
 
     return true
-
 end
 
 ## @lc code=end
