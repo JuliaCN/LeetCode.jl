2de1996db7160943ac285f1bb2c53436e21a67ef
Qling <41724569+rfhklwt@users.noreply.github.com>
2021-02-22 12:45:32 +0800

    Update 566.reshape-the-matrix.jl

--- test/problems/566.reshape-the-matrix.jl
+++ test/problems/566.reshape-the-matrix.jl
@@ -1,4 +1,4 @@
 @testset "566.reshape-the-matrix.jl" begin
-    @test matrixReshape([[1,2], [3,4]], 1, 4) == [[1,2,3,4]]
-    @test matrixReshape([[1,2], [3,4]], 2, 4) == [[1,2], [3,4]]
+    @test matrixReshape([[1, 2], [3, 4]], 1, 4) == [[1, 2, 3, 4]]
+    @test matrixReshape([[1, 2], [3, 4]], 2, 4) == [[1, 2], [3, 4]]
 end
