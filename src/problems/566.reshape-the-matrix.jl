a4801d86966a60561a7ac2240d54bcf726fac8c4
Qling <41724569+rfhklwt@users.noreply.github.com>
2021-02-22 12:54:56 +0800

    Add Problems 566 & 995 & 697 & 1438 & 766 (#81)
    * Add Problem 566
    
    * Update Author
    
    * Add Problem 995
    
    * Add Problem 697
    
    * Add Problem 697
    
    * remove comment
    
    * Add Problem 1438
    
    * Add Problem 766
    
    * Formatting Problem 766
    
    * Update 566.reshape-the-matrix.jl

--- src/problems/566.reshape-the-matrix.jl
+++ src/problems/566.reshape-the-matrix.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 566. Reshape the Matrix
 # id: problem566
-# author: Tian Jun
-# date: 2020-10-31
+# author: Qling
+# date: 2021-02-17
 # difficulty: Easy
 # categories: Array
 # link: <https://leetcode.com/problems/reshape-the-matrix/description/>
@@ -62,5 +62,18 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function matrixReshape(nums::Vector{Vector{Int}}, r::Int, c::Int)
+    m, n = length(nums), length(nums[1])
+
+    m * n != r * c && return nums
+
+    ans = [[0 for _ in 1:c] for _ in 1:r]
+
+    for x in 0:(m * n - 1)
+        ans[x ÷ c + 1][(x % c) + 1] = nums[x ÷ n + 1][(x % n) + 1]
+    end
+
+    return ans
+end
+
 ## @lc code=end
