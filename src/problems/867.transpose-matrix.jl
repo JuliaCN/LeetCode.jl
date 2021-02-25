cc72faf86d91cbc235b9f72b18de53091bf44721
Qling <rfhklwt@163.com>
2021-02-25 16:46:57 +0800

    Add Problem 867

--- src/problems/867.transpose-matrix.jl
+++ src/problems/867.transpose-matrix.jl
@@ -46,5 +46,15 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function transpose(matrix::Vector{Vector{Int}})::Vector{Vector{Int}}
+    m, n = length(matrix), length(matrix[1])
+
+    transpose_matrix = [[0 for _ = 1:m] for _ = 1:n]
+
+    for i = 1:m, j = 1:n
+        transpose_matrix[j][i] = matrix[i][j]
+    end
+
+    return transpose_matrix
+end
 ## @lc code=end
