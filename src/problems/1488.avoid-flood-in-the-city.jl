781b2010bd94e9abcf3685899b80c6500c621098
Qling <rfhklwt@163.com>
2021-02-26 12:41:40 +0800

    update solution

--- src/problems/1488.avoid-flood-in-the-city.jl
+++ src/problems/1488.avoid-flood-in-the-city.jl
@@ -99,13 +99,13 @@
 # 
 ## @lc code=start
 using LeetCode
-using BisectPy
+using DataStructures
 
 function avoid_flood(rains::Vector{Int})::Vector{Int}
     n = length(rains)
     options = [1 for _ = 1:n]
     seen = Dict{Int,Int}()
-    dry_days = []
+    dry_days = Vector{Int}()
 
     for (day, lake) in enumerate(rains)
         ## if no rain this day, record the day
@@ -117,7 +117,7 @@ function avoid_flood(rains::Vector{Int})::Vector{Int}
             ## if lake has been rained
             if haskey(seen, lake)
                 (isempty(dry_days) || dry_days[end] < seen[lake]) && return []
-                pos = bisect_left(dry_days, seen[lake])
+                pos = searchsortedfirst(dry_days, seen[lake])
                 options[dry_days[pos]] = lake
                 ## delete the dry_day we have used
                 deleteat!(dry_days, pos)
