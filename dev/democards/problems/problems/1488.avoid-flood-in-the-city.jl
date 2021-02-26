bdeec63cde38821b66bfd8ac7878b002b2228fee
Documenter.jl <documenter@juliadocs.github.io>
2021-02-26 06:05:30 +0000

    build based on d5cde41

--- dev/democards/problems/problems/1488.avoid-flood-in-the-city.jl
+++ dev/democards/problems/problems/1488.avoid-flood-in-the-city.jl
@@ -1,12 +1,12 @@
 # @lc code=start
 using LeetCode
-using BisectPy
+using DataStructures
 
 function avoid_flood(rains::Vector{Int})::Vector{Int}
     n = length(rains)
     options = [1 for _ = 1:n]
     seen = Dict{Int,Int}()
-    dry_days = []
+    dry_days = Vector{Int}()
 
     for (day, lake) in enumerate(rains)
         # if no rain this day, record the day
@@ -18,7 +18,7 @@ function avoid_flood(rains::Vector{Int})::Vector{Int}
             # if lake has been rained
             if haskey(seen, lake)
                 (isempty(dry_days) || dry_days[end] < seen[lake]) && return []
-                pos = bisect_left(dry_days, seen[lake])
+                pos = searchsortedfirst(dry_days, seen[lake])
                 options[dry_days[pos]] = lake
                 # delete the dry_day we have used
                 deleteat!(dry_days, pos)
@@ -29,6 +29,34 @@ function avoid_flood(rains::Vector{Int})::Vector{Int}
     end
 
     return options
+
+    # ## for bigger testcase, the following code is much better
+    # n = length(rains)
+    # options = fill(1, n)
+    # seen = Dict{Int,Int}()
+    # dry_days = SortedSet{Int}()
+
+    # for (day, lake) in enumerate(rains)
+    #     ## if no rain this day, record the day
+    #     if rains[day] == 0
+    #         push!(dry_days, day)
+    #         ## if rain this day
+    #     else
+    #         options[day] = -1
+    #         ## if lake has been rained
+    #         if haskey(seen, lake)
+    #             (isempty(dry_days) || last(dry_days) < seen[lake]) && return []
+    #             semitoken = searchsortedfirst(dry_days, seen[lake])
+    #             options[deref((dry_days, semitoken))] = lake
+    #             ## delete the dry_day we have used
+    #             delete!((dry_days, semitoken))
+    #         end
+
+    #         seen[lake] = day
+    #     end
+    # end
+
+    # return options
 end
 # @lc code=end
 
