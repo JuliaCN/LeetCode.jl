e59461f5d7b75b95c597b17558170f99bb319499
Qling <rfhklwt@163.com>
2021-03-05 12:45:25 +0800

    add more solution

--- test/problems/300.longest-increasing-subsequence.jl.jl
+++
@@ -1,5 +0,0 @@
-@testset "300.longest-increasing-subsequence.jl" begin
-    @test length_of_lis([10, 9, 2, 5, 3, 7, 101, 18]) == 4
-    @test length_of_lis([0, 1, 0, 3, 2, 3]) == 4
-    @test length_of_lis([7, 7, 7, 7, 7, 7, 7]) == 1
-end
