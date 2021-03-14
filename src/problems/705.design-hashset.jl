8b21ff059505f9040b13a8f32a03960fce379825
Qling <rfhklwt@163.com>
2021-03-14 22:08:31 +0800

    Fix error

--- src/problems/705.design-hashset.jl
+++ src/problems/705.design-hashset.jl
@@ -1,11 +1,77 @@
-@testset "705.design-hashset.jl" begin
-    hs = MyHashSet()
-    @test add_705!(hs, 1) == nothing
-    @test add_705!(hs, 2) == nothing
-    @test contains_705(hs, 1) == true
-    @test contains_705(hs, 3) == false
-    @test add_705!(hs, 2) == nothing
-    @test contains_705(hs, 2) == true
-    @test remove_705!(hs, 2) == []
-    @test contains_705(hs, 2) == false
+# ---
+# title: 705. Design HashSet
+# id: problem705
+# author: Qling
+# date: 2021-03-14
+# difficulty: Easy
+# categories: Hash Table, Design
+# link: <https://leetcode.com/problems/design-hashset/description/>
+# hidden: true
+# ---
+# 
+# Design a HashSet without using any built-in hash table libraries.
+# 
+# To be specific, your design should include these functions:
+# 
+#   * `add(value)`: Insert a value into the HashSet. 
+#   * `contains(value)` : Return whether the value exists in the HashSet or not.
+#   * `remove(value)`: Remove a value in the HashSet. If the value does not exist in the HashSet, do nothing.
+# 
+#   
+# **Example:**
+# 
+#     
+#     
+#     MyHashSet hashSet = new MyHashSet();
+#     hashSet.add(1);         
+#     hashSet.add(2);         
+#     hashSet.contains(1);    // returns true
+#     hashSet.contains(3);    // returns false (not found)
+#     hashSet.add(2);          
+#     hashSet.contains(2);    // returns true
+#     hashSet.remove(2);          
+#     hashSet.contains(2);    // returns false (already removed)
+#     
+# 
+#   
+# **Note:**
+# 
+#   * All values will be in the range of `[0, 1000000]`.
+#   * The number of operations will be in the range of `[1, 10000]`.
+#   * Please do not use the built-in HashSet library.
+# 
+# 
+## @lc code=start
+using LeetCode
+
+struct MyHashSet
+    buckets::Int
+    table::Vector{Vector{T}} where {T}
+    MyHashSet() = new(1009, [[] for _ = 1:1009])
 end
+
+hash_705(hash_set::MyHashSet, key::Int) = mod1(key, hash_set.buckets)
+
+function add_705!(hash_set::MyHashSet, key)
+    hash_key = hash_705(hash_set, key)
+
+    key ∈ hash_set.table[hash_key] && return
+
+    push!(hash_set.table[hash_key], key)
+    return
+end
+
+function remove_705!(hash_set::MyHashSet, key::Int)
+    hash_key = hash_705(hash_set, key)
+
+    key ∉ hash_set.table[hash_key] && return
+
+    deleteat!(hash_set.table[hash_key], hash_set.table[hash_key] .== key)
+end
+
+function contains_705(hash_set::MyHashSet, key::Int)
+    hash_key = hash_705(hash_set, key)
+
+    return key ∈ hash_set.table[hash_key]
+end
+## @lc code=end
--- src/problems/706.design-hashmap.jl
+++ src/problems/706.design-hashmap.jl
@@ -1,11 +1,93 @@
-@testset "706.design-hashmap.jl" begin
-    hm = MyHashMap()
-    @test put_706!(hm, 1, 1) == nothing
-    @test put_706!(hm, 2, 2) == nothing
-    @test get_706(hm, 1) == 1
-    @test get_706(hm, 3) == -1
-    @test put_706!(hm, 2, 1) == nothing
-    @test get_706(hm, 2) == 1
-    @test remove_706!(hm, 2) == nothing
-    @test get_706(hm, 2) == -1
+# ---
+# title: 706. Design HashMap
+# id: problem706
+# author: Qling
+# date: 2021-03-14
+# difficulty: Easy
+# categories: Hash Table, Design
+# link: <https://leetcode.com/problems/design-hashmap/description/>
+# hidden: true
+# ---
+# 
+# Design a HashMap without using any built-in hash table libraries.
+# 
+# To be specific, your design should include these functions:
+# 
+#   * `put(key, value)` : Insert a (key, value) pair into the HashMap. If the value already exists in the HashMap, update the value.
+#   * `get(key)`: Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+#   * `remove(key)` : Remove the mapping for the value key if this map contains the mapping for the key.
+# 
+#   
+# **Example:**
+# 
+#     
+#     
+#     MyHashMap hashMap = new MyHashMap();
+#     hashMap.put(1, 1);          
+#     hashMap.put(2, 2);         
+#     hashMap.get(1);            // returns 1
+#     hashMap.get(3);            // returns -1 (not found)
+#     hashMap.put(2, 1);          // update the existing value
+#     hashMap.get(2);            // returns 1 
+#     hashMap.remove(2);          // remove the mapping for 2
+#     hashMap.get(2);            // returns -1 (not found) 
+#     
+# 
+#   
+# **Note:**
+# 
+#   * All keys and values will be in the range of `[0, 1000000]`.
+#   * The number of operations will be in the range of `[1, 10000]`.
+#   * Please do not use the built-in HashMap library.
+# 
+# 
+## @lc code=start
+using LeetCode
+
+struct MyHashMap
+    buckets::Int
+    table::Vector{Vector{T}} where {T}
+    MyHashMap() = new(1009, [Tuple{Int,Int}[] for _ = 1:1009])
 end
+
+hash_706(hash_map::MyHashMap, key::Int) = mod1(key, hash_map.buckets)
+
+function put_706!(hash_map::MyHashMap, key::Int, value::Int)
+    hash_key = hash_706(hash_map, key)
+
+    if !isempty(hash_map.table[hash_key])
+        for (i, item) in enumerate(hash_map.table[hash_key])
+            if item[1] == key
+                hash_map.table[hash_key][i] = (key, value)
+                return
+            end
+        end
+    end
+
+    push!(hash_map.table[hash_key], (key, value))
+    return
+end
+
+function remove_706!(hash_map::MyHashMap, key)
+    hash_key = hash_706(hash_map, key)
+
+    if !isempty(hash_map.table[hash_key])
+        for (i, item) in enumerate(hash_map.table[hash_key])
+            if item[1] == key
+                deleteat!(hash_map.table[hash_key], i)
+                return
+            end
+        end
+    end
+end
+
+function get_706(hash_map::MyHashMap, key)
+    hash_key = hash_706(hash_map, key)
+
+    for item in hash_map.table[hash_key]
+        !isempty(item) && item[1] == key && return item[2]
+    end
+
+    return -1
+end
+## @lc code=end
