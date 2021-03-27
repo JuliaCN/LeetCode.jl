fcb88cbf54feb41681b217b88b90018840d837cf
Qling <rfhklwt@163.com>
2021-03-27 10:04:15 +0800

    Format file

--- src/problems/73.set-matrix-zeroes.jl
+++ src/problems/73.set-matrix-zeroes.jl
@@ -58,28 +58,28 @@ function set_zeroes(matrix::Vector{Vector{Int}})::Vector{Vector{Int}}
     is_col = false
     m, n = length(matrix), length(matrix[1])
 
-    for i in 1:m
+    for i = 1:m
         matrix[i][1] == 0 && (is_col = true)
-        
-        for j in 2:n
+
+        for j = 2:n
             matrix[i][j] == 0 && (matrix[i][1] = matrix[1][j] = 0)
         end
     end
 
-    for i in 2:m, j in 2:n
+    for i = 2:m, j = 2:n
         (matrix[i][1] == 0 || matrix[1][j] == 0) && (matrix[i][j] = 0)
     end
 
     ## See if the first row needs to be set to zero as well
     if matrix[1][1] == 0
-        for j in 1: n
+        for j = 1:n
             matrix[1][j] = 0
         end
     end
 
     ## See if the first column needs to be set to zero as well 
     if is_col
-        for i in 1:m
+        for i = 1:m
             matrix[i][1] = 0
         end
     end
