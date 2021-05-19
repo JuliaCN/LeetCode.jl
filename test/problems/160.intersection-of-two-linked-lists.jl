e2ab0c8ad981525bec0ba37dce5fc5bb52ec40d3
Qling <rfhklwt@163.com>
2021-05-19 18:10:23 +0800

    Add problem 160

---
+++ test/problems/160.intersection-of-two-linked-lists.jl
@@ -0,0 +1,5 @@
+@testset "160.intersection-of-two-linked-lists.jl" begin
+    @test get_intersection_node(ListNode{Int}([4, 2, 8, 4, 5]), ListNode{Int}([5, 0, 1, 8, 4, 5])) == ListNode{Int}([8, 4, 5])
+    @test get_intersection_node(ListNode{Int}([0, 9, 1, 2, 4]), ListNode{Int}([3, 2, 4])) == ListNode{Int}([2, 4])
+    @test get_intersection_node(ListNode{Int}([2, 6, 4]), ListNode{Int}([1, 5])) == nothing
+end
