a558d3be5356deb2db647aeba4b74ca1afc593ca
Qling <rfhklwt@163.com>
2021-05-28 21:09:53 +0800

    fix bug

--- test/problems/30.substring-with-concatenation-of-all-words.jl
+++ test/problems/30.substring-with-concatenation-of-all-words.jl
@@ -1,5 +1,5 @@
 @testset "30.substring-with-concatenation-of-all-words.jl" begin
-    @test find_word_concatenation("barfoothefoobarman", ["foo","bar"]) == [1, 10]
-    @test find_word_concatenation("wordgoodgoodgoodbestword", ["word","good","best","word"]) == Int[]
-    @test find_word_concatenation("barfoofoobarthefoobarman", ["bar","foo","the"]) == [7, 10, 13]
+    @test find_substring("barfoothefoobarman", ["foo","bar"]) == [1, 10]
+    @test find_substring("wordgoodgoodgoodbestword", ["word","good","best","word"]) == Int[]
+    @test find_substring("barfoofoobarthefoobarman", ["bar","foo","the"]) == [7, 10, 13]
 end
