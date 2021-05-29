a52e9016888929bcdfe03c93c73e9b7a39945bb0
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-05-29 19:21:34 +0800

    Solution to 363

--- src/problems/1074.number-of-submatrices-that-sum-to-target.jl
+++ src/problems/1074.number-of-submatrices-that-sum-to-target.jl
@@ -70,10 +70,10 @@ function num_submatrix_sum_target(matrix::AbstractMatrix{Int}, target::Int)
         pref_sum[i, j] = pref_sum[i - 1, j] + pref_sum[i, j - 1] + matrix[i, j] - pref_sum[i - 1, j - 1]
     end
     res = 0
-    for top in 1:m, bot in top:m
+    for tp in 1:m, bot in tp:m
         sum_cnt = Dict{Int, Int}()
         for r in 1:n
-            rows_sum = pref_sum[bot, r] - pref_sum[top - 1, r]
+            rows_sum = pref_sum[bot, r] - pref_sum[tp - 1, r]
             rows_sum == target && (res += 1)
             res += get(sum_cnt, rows_sum - target, 0)
             sum_cnt[rows_sum] = get(sum_cnt, rows_sum, 0) + 1
--- src/problems/363.max-sum-of-rectangle-no-larger-than-k.jl
+++ src/problems/363.max-sum-of-rectangle-no-larger-than-k.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 363. Max Sum of Rectangle No Larger Than K
 # id: problem363
-# author: Tian Jun
-# date: 2020-10-31
+# author: Indigo
+# date: 2021-05-29
 # difficulty: Hard
 # categories: Binary Search, Dynamic Programming, Queue
 # link: <https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/description/>
@@ -30,5 +30,25 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function max_sum_submatrix(matrix::AbstractMatrix{Int}, k::Int)
+    m, n = size(matrix)
+    m > n && return max_sum_submatrix(matrix', k)
+    pref_sum = OffsetArray(fill(0, m + 1, n + 1), -1, -1)
+
+    for i in 1:m, j in 1:n
+        pref_sum[i, j] = pref_sum[i - 1, j] + pref_sum[i, j - 1] + matrix[i, j] -
+                         pref_sum[i - 1, j - 1]
+    end
+    res = typemin(Int)
+    for tp in 1:m, bot in tp:m
+        ss = SortedSet{Int}()
+        push!(ss, 0)
+        for r in 1:n
+            rows_sum = pref_sum[bot, r] - pref_sum[tp - 1, r]
+            res = max(res, rows_sum - deref((ss, searchsortedfirst(ss, rows_sum - k))))
+            push!(ss, rows_sum)
+        end
+    end
+    return res
+end
 ## @lc code=end
