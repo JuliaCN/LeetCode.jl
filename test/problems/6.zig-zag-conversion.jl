cc68c38f75414159a0d0f50b0e3af529e6904ee6
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-05-30 15:33:14 +0800

    codecov

--- test/problems/6.zig-zag-conversion.jl
+++ test/problems/6.zig-zag-conversion.jl
@@ -1,4 +1,5 @@
 @testset "6.zig-zag-conversion.jl" begin
     @test zig_zag_convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI"
     @test zig_zag_convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR"
+    @test zig_zag_convert("P", 1) == "P"
 end
