db0ad182f8c282cddb29027bc744e3646b3b5074
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-05-30 10:27:22 +0800

    Solution to 221

--- test/problems/209.minimum-size-subarray-sum.jl
+++ test/problems/209.minimum-size-subarray-sum.jl
@@ -1,8 +1,6 @@
-@testset "204.count-primes.jl" begin
-    @test min_subarray_len(7, [2,3,1,2,4,3]) == 2
-    @test min_subarray_len(5, [2,3,1,2,4,3]) == 2
-    @test min_subarray_len(3, [2,3,1,2,4,3]) == 1
-    @test min_subarray_len(15, [2,3,1,2,4,3]) == 6
+@testset "209.minimum-size-subarray-sum.jl" begin
+    @test min_subarray_len(7, [2, 3, 1, 2, 4, 3]) == 2
+    @test min_subarray_len(5, [2, 3, 1, 2, 4, 3]) == 2
+    @test min_subarray_len(3, [2, 3, 1, 2, 4, 3]) == 1
+    @test min_subarray_len(15, [2, 3, 1, 2, 4, 3]) == 6
 end
-
- 
