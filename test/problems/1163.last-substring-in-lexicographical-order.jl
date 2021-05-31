753c14bf30cf43f2a019a77f92511bb5c9687d6e
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-05-31 22:46:53 +0800

    fix bugs

--- test/problems/1163.last-substring-in-lexicographical-order.jl
+++ test/problems/1163.last-substring-in-lexicographical-order.jl
@@ -1,10 +1,11 @@
 @testset "1163.last-substring-in-lexicographical-order.jl" begin
-    @test last_substring(
-        "arpzrkqizlvqouudiggnezejhhuaanmaickndesynbkscubboihueuatpbhclzifkrcjhncllfdyqeawmdcefyhcnzciypnrezcu",
-    ) ==
+    @test last_substring("arpzrkqizlvqouudiggnezejhhuaanmaickndesynbkscubboihueuatpbhclzifkrcjhncllfdyqeawmdcefyhcnzciypnrezcu") ==
           "zrkqizlvqouudiggnezejhhuaanmaickndesynbkscubboihueuatpbhclzifkrcjhncllfdyqeawmdcefyhcnzciypnrezcu"
     @test last_substring("abab") == "bab"
     @test last_substring("leetcode") == "tcode"
     @test last_substring("cccc") == "cccc"
     @test last_substring("ccacccacccc") == "cccc"
+    @test last_substring("caccacccaa") == "cccaa"
+    s = "a" ^ 4000000
+    @test last_substring(s) == s
 end
