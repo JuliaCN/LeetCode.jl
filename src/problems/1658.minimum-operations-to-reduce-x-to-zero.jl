01fdb1b2465aa0394eb020d0d8a31642f728d7da
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-05-31 22:36:22 +0800

    codecov

--- src/problems/1658.minimum-operations-to-reduce-x-to-zero.jl
+++ src/problems/1658.minimum-operations-to-reduce-x-to-zero.jl
@@ -58,53 +58,18 @@
 using LeetCode
 
 function min_operations(nums::Vector{Int}, x::Int)
-    rnums = reverse(nums)
-    if nums[1] > x && rnums[1] > x
-        return -1
-    end
+    target = sum(nums) - x
+    target < 0 && return -1
+    println(target)
+    left, right, maxn = 0, 0, -1
     len = length(nums)
-    rlm, llm, res = len, len, len
-    for i in 1:(len - 1)
-        if ((nums[i + 1] += nums[i]) > x)
-            rlm = i
-            break
+    while right < len
+        target -= nums[right += 1]
+        while target < 0
+            target += nums[left += 1]
         end
+        (target == 0) && (maxn = max(maxn, right - left))
     end
-    if rm == len
-        if nums[end] < x
-            return -1
-        else
-            return len
-        end
-    end
-    for i in 1:(len - 1)
-        if ((rnums[i + 1] += rnums[i]) > x)
-            llm = i
-            break
-        end
-    end
-    i, j = 1, llm
-    while i <= rlm && j > 0
-        if nums[i] == x
-            res = min(i, res)
-            i += 1
-        elseif rnums[j] == x
-            res = min(j, res)
-            j -= 1
-        else
-            sm = nums[i] + rnums[j]
-            ## println(i," ", j," ", sm)
-            if sm == x
-                res = min(i + j, res)
-                i += 1
-                j -= 1
-            elseif sm > x
-                j -= 1
-            else
-                i += 1
-            end
-        end
-    end
-    return res
+    maxn == -1 ? maxn : len - maxn
 end
 ## @lc code=end
