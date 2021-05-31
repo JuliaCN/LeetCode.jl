181583d2db78f449988fd1d92a81c945481d3063
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-05-31 23:19:48 +0800

    fix bugs

--- test/problems/81.search-in-rotated-sorted-array-ii.jl
+++ test/problems/81.search-in-rotated-sorted-array-ii.jl
@@ -2,7 +2,7 @@
     @test search([2, 5, 6, 0, 0, 1, 2], 0) == true
     @test search([2, 5, 6, 0, 0, 1, 2], 3) == false
     @test search([2, 5, 6, 0, 0, 1, 2], 2) == true
-    @test search([2, 5, 6, 0, 0, 1, 2], 1) == false
+    @test search([2, 5, 6, 0, 0, 1, 2], 1) == true
     @test search([2, 5, 6, 0, 0, 1, 2], 4) == false
     @test search([2, 5, 6, 0, 0, 1, 2], 5) == true
     @test search([2, 5, 6, 0, 0, 1, 2], 6) == true
