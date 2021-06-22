7a069cae107fe87f88e2e3debb7c6d98063827de
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-06-22 09:45:30 +0800

    prefix sum instead of hash table

--- src/problems/768.max-chunks-to-make-sorted-ii.jl
+++ src/problems/768.max-chunks-to-make-sorted-ii.jl
@@ -55,25 +55,26 @@
 using LeetCode
 
 function max_chunks_to_sorted(arr::Vector{Int})::Int
-    count = DefaultDict(1)
-    non_zeros, res = 0, 0
-
+    ##count = DefaultDict(1)
+    ##non_zeros, res = 0, 0
+    res = s1 = s2 = 0
     for (a, b) in zip(arr, sort(arr))
-        count[a] += 1
-        if count[a] == 0
-            non_zeros -= 1
-        elseif count[a] == 1
-            non_zeros += 1
-        end
-
-        count[b] -= 1
-        if count[b] == 0
-            non_zeros -= 1
-        elseif count[b] == -1
-            non_zeros += 1
-        end
-
-        (non_zeros == 0) && (res += 1)
+        s1 += a
+        s2 += b
+        (s1 == s2) && (res += 1)
+        ## count[a] += 1
+        ## if count[a] == 0
+        ##     non_zeros -= 1
+        ## elseif count[a] == 1
+        ##     non_zeros += 1
+        ## end
+        ## count[b] -= 1
+        ## if count[b] == 0
+        ##     non_zeros -= 1
+        ## elseif count[b] == -1
+        ##     non_zeros += 1
+        ## end
+        ## (non_zeros == 0) && (res += 1)
     end
 
     return res
