9cd63197e1b4fd8ecb408f97d09d55f52a00cd6d
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-06-24 10:58:22 +0800

    codecov

--- src/problems/777.swap-adjacent-in-lr-string.jl
+++ src/problems/777.swap-adjacent-in-lr-string.jl
@@ -78,30 +78,16 @@ using LeetCode
 function can_transform(s::String, t::String)
     i, j = 1, 1
     len = length(s)
-    if len != length(t)
-        return false
-    end
-    while i <= len && j <= len
-        while s[i] == 'X' && i <= len
-            i += 1
-        end
-        while t[j] == 'X' && j <= len
-            j += 1
-        end
+    len != length(t) && return false
+    while i <= len || j <= len
+        while i <= len && s[i] == 'X'; i += 1; end
+        while j <= len && t[j] == 'X' ; j += 1; end
+        ((i <= len) ⊻ (j <= len)) && return false
         if i <= len && j <= len
-            if s[i] != t[j] || s[i] == 'L' && i < j || s[i] == 'R' && i > j
-                return false
-            end
+            (s[i] != t[j] || (s[i] == 'L' && i < j) || (s[i] == 'R' && i > j)) && return false
         end
-        i += 1
-        j += 1
+        i += 1; j += 1
     end
-    while i <= len && s[i] == 'X'
-        i += 1
-    end
-    while j <= len && t[j] == 'X'
-        j += 1
-    end
-    return i == j
+    return true
 end
 ## @lc code=end
