0feacf1471664c0354fffecf8b3925d1c3b5044f
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-06-26 22:03:41 +0800

    Solution to 583

--- src/problems/583.delete-operation-for-two-strings.jl
+++ src/problems/583.delete-operation-for-two-strings.jl
@@ -31,5 +31,6 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+min_distance583(word1::String, word2::String) = length(word2) + length(word2) - 2longest_common_subsequence(word1, word2)
+    
 ## @lc code=end
