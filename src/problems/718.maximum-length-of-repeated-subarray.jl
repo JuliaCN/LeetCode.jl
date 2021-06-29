f5d2225604f17c6100d63c8bfe43ffebee834dc0
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-06-29 20:13:07 +0800

    Solution to 718

--- src/problems/718.maximum-length-of-repeated-subarray.jl
+++ src/problems/718.maximum-length-of-repeated-subarray.jl
@@ -37,6 +37,15 @@
 ## @lc code=start
 using LeetCode
 
-find_length718(nums1::Vector{Int}, nums2::Vector{Int}) = 
-    longest_common_subsequence(nums1, nums2)
+function longest_common_substring(itr1, itr2)
+    m, n = length(itr1) + 1, length(itr2) + 1
+    dp = fill(0, m, n)
+    for i in 2: m, j in 2: n
+        (itr1[i - 1] == itr2[j - 1]) && (dp[i, j] = dp[i - 1, j - 1] + 1)
+    end
+    return maximum(dp[m, n])
+end
+
+find_length(nums1::Vector{Int}, nums2::Vector{Int}) = 
+    longest_common_substring(nums1, nums2)
 ## @lc code=end
