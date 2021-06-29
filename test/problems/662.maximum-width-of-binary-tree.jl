1f11df0b5d0efb6cdcc95bf8820ecd6115f5ebf2
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-06-29 10:27:11 +0800

    codecov

--- test/problems/662.maximum-width-of-binary-tree.jl
+++ test/problems/662.maximum-width-of-binary-tree.jl
@@ -1,4 +1,5 @@
 @testset "662.maximum-width-of-binary-tree.jl" begin
+    @test width_of_binary_tree(nothing) == 0
     @test width_of_binary_tree(TreeNode{Int}([1, 3, 2, 5, 3, nothing, 9])) == 4
     @test width_of_binary_tree(TreeNode{Int}([1, 3, nothing, 5, 3])) == 2
     @test width_of_binary_tree(TreeNode{Int}([1, 3, 2, 5])) == 2
