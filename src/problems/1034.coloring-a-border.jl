3faed3a9f32ca061485845019fadc491396b366c
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-07-31 10:11:29 +0800

    Solution to 1034

--- src/problems/1034.coloring-a-border.jl
+++ src/problems/1034.coloring-a-border.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 1034. Coloring A Border
 # id: problem1034
-# author: Tian Jun
-# date: 2020-10-31
+# author: Indigo
+# date: 2021-07-31
 # difficulty: Medium
 # categories: Depth-first Search
 # link: <https://leetcode.com/problems/coloring-a-border/description/>
@@ -63,5 +63,27 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function color_border(grid::Matrix{Int}, r0::Int, c0::Int, color::Int)
+    visited = fill(false, size(grid))
+    cis = CartesianIndices(grid)
+    dirs = CartesianIndex.(((-1, 0), (1, 0), (0, -1), (0, 1)))
+    cs = CartesianIndex{2}[]
+    function dfs(grid::Matrix{Int}, pos)
+        visited[pos] = true
+        cnt = 0
+        for d in dirs
+            I = pos + d
+            if I ∈ cis && grid[I] == grid[pos]
+                cnt += 1
+                !visited[I] && dfs(grid, I)
+            end
+        end
+        (cnt != 4) && push!(cs, pos) 
+    end
+    dfs(grid, CartesianIndex(r0, c0))
+    for I in cs
+        grid[I] = color
+    end
+    grid
+end
 ## @lc code=end
