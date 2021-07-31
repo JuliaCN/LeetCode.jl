515b806f6eb84aa21bf24bc4d2aec7b1276daeba
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-07-31 09:53:48 +0800

    Solution to 1035

--- src/problems/1035.uncrossed-lines.jl
+++ src/problems/1035.uncrossed-lines.jl
@@ -64,5 +64,5 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+max_uncrossed_lines(nums1::Vector{Int}, nums2::Vector{Int}) = longest_common_subsequence(nums1, nums2)
 ## @lc code=end
