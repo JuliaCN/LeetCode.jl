4bbbfe5a3d2f2cdd3529e2c7a46aac4d37954892
Indigo <58110312+AquaIndigo@users.noreply.github.com>
2021-08-02 17:14:44 +0800

    Update 1035.uncrossed-lines.jl

--- src/problems/1035.uncrossed-lines.jl
+++ src/problems/1035.uncrossed-lines.jl
@@ -64,5 +64,6 @@
 ## @lc code=start
 using LeetCode
 
-max_uncrossed_lines(nums1::Vector{Int}, nums2::Vector{Int}) = longest_common_subsequence(nums1, nums2)
+max_uncrossed_lines(nums1::Vector{Int}, nums2::Vector{Int}) = 
+    longest_common_subsequence(nums1, nums2)
 ## @lc code=end
