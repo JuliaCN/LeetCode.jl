14c13e47baeb9dcaa642577b71f980aabe57de2b
Jimmy Shen <jmmshn@gmail.com>
2021-08-11 16:51:58 -0700

    added 954

--- src/problems/954.array-of-doubled-pairs.jl
+++ src/problems/954.array-of-doubled-pairs.jl
@@ -61,4 +61,14 @@
 using LeetCode
 
 ## add your code here:
+function canReorderDoubled(arr::Vector{Int})::Bool
+    cnt = counter(arr)
+    for a in sort!(arr, by=abs)
+        cnt[a] == 0 && continue
+        cnt[2a] == 0 && return false
+        cnt[2a] -= 1
+        cnt[a] -= 1
+    end
+    return true
+end
 ## @lc code=end
