a0212b0fb9e61fafb77533f1ff5fdc89b1e6ed16
Jimmy Shen <jmmshn@gmail.com>
2021-08-18 17:28:26 -0700

    56

--- src/problems/56.merge-intervals.jl
+++ src/problems/56.merge-intervals.jl
@@ -45,5 +45,15 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function mergeIntervals(intervals::Vector{Vector{Int}})::Vector{Vector{Int}}
+    result = Vector{Vector{Int}}([])
+    for (a, b) in sort!(intervals, by = i->i[1])
+        if size(result,1) > 0 && result[end][2] >= a 
+            result[end][2] = max(result[end][2], b)
+        else
+            push!(result, [a,b])
+        end
+    end
+    return result
+end
 ## @lc code=end
