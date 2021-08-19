12fdba6ee5cce0662aac49085e2c0a053de9a3fa
Indigo <j.c.f.gauss@sjtu.edu.cn>
2021-08-19 09:17:32 +0800

    rename the method

--- test/problems/1035.uncrossed-lines.jl
+++ test/problems/1035.uncrossed-lines.jl
@@ -1,5 +1,5 @@
 @testset "1035.uncrossed-lines.jl" begin
-    @test max_uncrossed_lines([1,4,2], [1,2,4]) == 2
-    @test max_uncrossed_lines([2,5,1,2,5], [10,5,2,1,5,2]) == 3
-    @test max_uncrossed_lines([1,3,7,1,7,5], [1,9,2,5,1]) == 2
+    @test max_uncrossed_lines([1, 4, 2], [1, 2, 4]) == 2
+    @test max_uncrossed_lines([2, 5, 1, 2, 5], [10, 5, 2, 1, 5, 2]) == 3
+    @test max_uncrossed_lines([1, 3, 7, 1, 7, 5], [1, 9, 2, 5, 1]) == 2
 end
