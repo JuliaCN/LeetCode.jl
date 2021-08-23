c2b64d2158acbfff3d65f4be4d046f4a7be948ab
Indigo <j.c.f.gauss@sjtu.edu.cn>
2021-08-23 10:42:20 +0800

    fix bug

--- src/problems/72.edit-distance.jl
+++ src/problems/72.edit-distance.jl
@@ -59,25 +59,19 @@ using LeetCode
 
 function min_distance(word1::String, word2::String)::Int32
     m, n = length(word1) + 1, length(word2) + 1
-    dp = fill(0, m, n)
+    dp = zeros(Int32, m, n)
+    dp[:, 1] .= (1:m) .- 1
+    dp[1, :] .= (1:n) .- 1
 
-    for i = 1:m
-        dp[i, 1] = i - 1
+    for i in 2:m, j in 2:n
+        dp[i, j] =
+            1 + min(
+                dp[i - 1, j - 1] - Int(word1[i - 1] == word2[j - 1]),
+                dp[i, j - 1],
+                dp[i - 1, j],
+            )
     end
-
-    for j = 1:n
-        dp[1, j] = j - 1
-    end
-
-    for i = 2:m, j = 2:n
-        if word1[i-1] == word2[j-1]
-            dp[i, j] = dp[i-1, j-1]
-        else
-            dp[i, j] = min(dp[i-1, j-1], dp[i, j-1], dp[i-1, j]) + 1
-        end
-    end
-
-    return dp[m, n]
+    return dp[end, end]
 end
 
 ## @lc code=end
