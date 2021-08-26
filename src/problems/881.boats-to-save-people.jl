5baa3f0334d7aaad43ff40de163a85eb376b816a
Indigo <j.c.f.gauss@sjtu.edu.cn>
2021-08-26 20:21:34 +0800

    Solution to 881

--- src/problems/881.boats-to-save-people.jl
+++ src/problems/881.boats-to-save-people.jl
@@ -55,5 +55,16 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function num_rescue_boats(people::Vector{Int}, limit::Int)
+    sort!(people)
+    a, b = 1, length(people)
+    res = 0
+    while a <= b
+        res += 1
+        a == b && break
+        b -= 1
+        (people[a] + people[b] <= limit) && (a += 1)
+    end
+    res
+end
 ## @lc code=end
