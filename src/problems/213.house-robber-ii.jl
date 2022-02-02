01931373d6781611c51ce4b2af1d7077b51564b3
rex <1073853456@qq.com>
2022-02-02 18:09:59 +0800

    solution to 213

--- src/problems/213.house-robber-ii.jl
+++ src/problems/213.house-robber-ii.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 213. House Robber II
 # id: problem213
-# author: Tian Jun
-# date: 2020-10-31
+# author: zhwang
+# date: 2022-01-19
 # difficulty: Medium
 # categories: Dynamic Programming
 # link: <https://leetcode.com/problems/house-robber-ii/description/>
@@ -60,5 +60,19 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function rob_213(nums::Vector{Int})::Int
+    length(nums) <= 3 && return max(nums...)
+    ## start from house 1
+    a, b1 = nums[1], nums[1] + nums[3]
+    for num in @view nums[4:(end - 1)]
+        a, b1 = b1, max(b1, a + num)
+    end
+    ## start from house 2
+    a, b2 = nums[2], max(nums[2], nums[3])
+    for num in @view nums[4:end]
+        a, b2 = b2, max(b2, a + num)
+    end
+    return max(b1, b2)
+end
+
 ## @lc code=end
