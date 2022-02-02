cd530d1570ed24ca648d3e9c6cec2b6477744f22
rex <1073853456@qq.com>
2022-02-02 19:49:44 +0800

    solution to 350

--- src/problems/350.intersection-of-two-arrays-ii.jl
+++ src/problems/350.intersection-of-two-arrays-ii.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 350. Intersection of Two Arrays II
 # id: problem350
-# author: Tian Jun
-# date: 2020-10-31
+# author: zhwang
+# date: 2022-01-19
 # difficulty: Easy
 # categories: Hash Table, Two Pointers, Binary Search, Sort
 # link: <https://leetcode.com/problems/intersection-of-two-arrays-ii/description/>
@@ -41,5 +41,22 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function intersection_of_arrays(nums1::Vector{Int}, nums2::Vector{Int})::Vector{Int}
+    nums1, nums2 = sort(nums1), sort(nums2)
+    n1, n2 = length(nums1), length(nums2)
+    i1, i2, res = 1, 1, Int[]
+    while i1 <= n1 && i2 <= n2
+        if nums1[i1] > nums2[i2]
+            i2 += 1
+        elseif nums1[i1] < nums2[i2]
+            i1 += 1
+        else
+            push!(res, nums1[i1])
+            i1 += 1
+            i2 += 1
+        end
+    end
+    return res
+end
+
 ## @lc code=end
