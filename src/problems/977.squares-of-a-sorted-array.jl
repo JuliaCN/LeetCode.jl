35c35b52cd5bfc8e429ba30d463811bc9627b533
rex <1073853456@qq.com>
2022-02-03 00:42:35 +0800

    solution to 977

--- src/problems/977.squares-of-a-sorted-array.jl
+++ src/problems/977.squares-of-a-sorted-array.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 977. Squares of a Sorted Array
 # id: problem977
-# author: Tian Jun
-# date: 2020-10-31
+# author: zhwang
+# date: 2022-01-19
 # difficulty: Easy
 # categories: Array, Two Pointers
 # link: <https://leetcode.com/problems/squares-of-a-sorted-array/description/>
@@ -44,5 +44,31 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+## binary search + merge sort
+function squares_of_a_sorted_array(nums::Vector{Int})::Vector{Int}
+    n = length(nums)
+    ## binary search
+    left, right = 1, n
+    while left <= right
+        mid = (right + left) >> 1
+        if nums[mid] < 0
+            left = mid + 1
+        else
+            right = mid - 1
+        end
+    end
+    ## merge sort
+    i, j, res = right, left, Int[]
+    while i >= 1 && j <= n
+        if nums[i]^2 < nums[j]^2
+            push!(res, nums[i]^2)
+            i -= 1
+        else
+            push!(res, nums[j]^2)
+            j += 1
+        end
+    end
+    return vcat(res, (i == 0) ? nums[j:end] .^ 2 : nums[i:-1:1] .^ 2)
+end
+
 ## @lc code=end
