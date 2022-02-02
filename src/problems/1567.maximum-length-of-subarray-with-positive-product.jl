af3015b63de724e24ccb308366ee80a342d37b13
rex <1073853456@qq.com>
2022-02-03 01:00:37 +0800

    solution to 1567

--- src/problems/1567.maximum-length-of-subarray-with-positive-product.jl
+++ src/problems/1567.maximum-length-of-subarray-with-positive-product.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 1567. Maximum Length of Subarray With Positive Product
 # id: problem1567
-# author: Tian Jun
-# date: 2020-10-31
+# author: zhwang
+# date: 2022-01-23
 # difficulty: Medium
 # categories: Greedy
 # link: <https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product/description/>
@@ -73,5 +73,22 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function get_max_len(nums::Vector{Int})::Int
+    pos_pre = neg_pre = max_ans = 0
+    for num in nums
+        if num == 0
+            pos_pre = neg_pre = 0
+        elseif num > 0
+            pos_pre = pos_pre + 1
+            neg_pre = neg_pre != 0 ? neg_pre + 1 : 0
+        else
+            neg_tmp = neg_pre
+            neg_pre = pos_pre != 0 ? pos_pre + 1 : 1
+            pos_pre = neg_tmp != 0 ? neg_tmp + 1 : 0
+        end
+        max_ans = max(max_ans, pos_pre)
+    end
+    return max_ans
+end
+
 ## @lc code=end
