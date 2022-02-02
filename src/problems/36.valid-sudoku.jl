997f43a9802e299fa9b377b965f1b881d8a77267
rex <1073853456@qq.com>
2022-02-02 13:47:27 +0800

    solution to 36

--- src/problems/36.valid-sudoku.jl
+++ src/problems/36.valid-sudoku.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 36. Valid Sudoku
 # id: problem36
-# author: Tian Jun
-# date: 2020-10-31
+# author: zhwang
+# date: 2022-01-23
 # difficulty: Medium
 # categories: Hash Table
 # link: <https://leetcode.com/problems/valid-sudoku/description/>
@@ -73,5 +73,21 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function is_valid_sudoku(board::Vector{Vector{Char}})::Bool
+    function check_line(i::Int)::Bool
+        s = replace(join(board[i]), "." => "")
+        return length(s) == length(Set{Char}(s))
+    end
+    function check_col(i::Int)::Bool
+        s = replace(join(board[k][i] for k in 1:9), '.' => "")
+        return length(s) == length(Set{Char}(s))
+    end
+    function check_box(i::Int, j::Int)::Bool
+        s = replace(join(board[i + p1][j + p2] for p1 in 1:3 for p2 in 1:3), "." => "")
+        return length(s) == length(Set{Char}(s))
+    end
+    return all(check_line(i) && check_col(i) for i in 1:9) &&
+           all(check_box(3 * (i - 1), 3 * (j - 1)) for i in 1:3 for j in 1:3)
+end
+
 ## @lc code=end
