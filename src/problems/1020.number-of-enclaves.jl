f816a754bba51e3ab96a4cf2444ceb60a347ffd3
rex <1073853456@qq.com>
2022-02-17 12:04:40 +0800

    solution to 1020

--- src/problems/1020.number-of-enclaves.jl
+++ src/problems/1020.number-of-enclaves.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 1020. Number of Enclaves
 # id: problem1020
-# author: Tian Jun
-# date: 2020-10-31
+# author: zhwang
+# date: 2022-02-12
 # difficulty: Medium
 # categories: Depth-first Search
 # link: <https://leetcode.com/problems/number-of-enclaves/description/>
@@ -52,5 +52,33 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+## BFS algorithm
+function num_of_enclaves_bfs(grid::Vector{Vector{Int}})::Int
+    m, n, queue = length(grid), length(grid[1]), Tuple{Int,Int}[]
+    (m <= 2 || n <= 2) && return 0
+    for i in 1:m ## left and right border
+        grid[i][1] == 1 && push!(queue, (i, 1))
+        grid[i][n] == 1 && push!(queue, (i, n))
+    end
+    for i in 2:(n - 1)
+        grid[1][i] == 1 && push!(queue, (1, i))
+        grid[m][i] == 1 && push!(queue, (m, i))
+    end
+    walkoff = copy(queue)
+    while length(queue) != 0
+        i, j = popfirst!(queue)
+        for (a, b) in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+            if a >= 1 &&
+                b >= 1 &&
+                a <= m &&
+                b <= n &&
+                grid[a][b] != 0 &&
+                !((a, b) in walkoff)
+                push!(walkoff, (a, b))
+                push!(queue, (a, b))
+            end
+        end
+    end
+    return sum(sum(line) for line in grid) - length(walkoff)
+end
 ## @lc code=end
--- src/problems/567.permutation-in-string.jl
+++ src/problems/567.permutation-in-string.jl
@@ -45,8 +45,11 @@ using LeetCode
 
 ## Double Pointers
 function check_inclusion(s1::String, s2::String)::Bool
-    empty!(d::Dict{Char,Int}) = for i in 'a':'z' d[i] = 0 end;
-    count1, count2 = Dict{Char,Int}(), Dict{Char,Int}(i=>0 for i in 'a':'z')
+    empty!(d::Dict{Char,Int}) =
+        for i in 'a':'z'
+            d[i] = 0
+        end
+    count1, count2 = Dict{Char,Int}(), Dict{Char,Int}(i => 0 for i in 'a':'z')
     start, left = true, 1
     for i in s1
         count1[i] = haskey(count1, i) ? count1[i] + 1 : 1
@@ -57,7 +60,7 @@ function check_inclusion(s1::String, s2::String)::Bool
             empty!(count2)
         elseif count2[c] >= count1[c]
             for j in left:i
-                s2[j] == c && (left = j + 1;break)
+                s2[j] == c && (left = j + 1; break)
                 count2[s2[j]] -= 1
             end
         else
