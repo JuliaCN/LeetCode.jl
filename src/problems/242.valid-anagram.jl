1eeae092f9bb981a8decf2b6616bc897ad5681d4
rex <1073853456@qq.com>
2022-02-17 11:57:36 +0800

    solution to 242

--- src/problems/242.valid-anagram.jl
+++ src/problems/242.valid-anagram.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 242. Valid Anagram
 # id: problem242
-# author: Tian Jun
-# date: 2020-10-31
+# author: zhwang
+# date: 2022-02-15
 # difficulty: Easy
 # categories: Hash Table, Sort
 # link: <https://leetcode.com/problems/valid-anagram/description/>
@@ -38,6 +38,15 @@
 # 
 ## @lc code=start
 using LeetCode
+using DataStructures
+function is_anagram(s::String, t::String)::Bool
+    length(s) != length(t) && return false
+    count_s, count_t = DefaultDict{Char,Int}(0), DefaultDict{Char,Int}(0)
+    for i in eachindex(s)
+        count_s[s[i]] += 1
+        count_t[t[i]] += 1
+    end
+    return all(count_s[c] == count_t[c] for c in 'a':'z')
+end
 
-## add your code here:
 ## @lc code=end
