8f93f25e51469894ceeaaa3528e318332d2f65fb
rex <1073853456@qq.com>
2022-02-17 12:07:23 +0800

    solution to 1380

--- src/problems/1380.lucky-numbers-in-a-matrix.jl
+++ src/problems/1380.lucky-numbers-in-a-matrix.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 1380. Lucky Numbers in a Matrix
 # id: problem1380
-# author: Tian Jun
-# date: 2020-10-31
+# author: zhwang
+# date: 2022-02-15
 # difficulty: Easy
 # categories: Array
 # link: <https://leetcode.com/problems/lucky-numbers-in-a-matrix/description/>
@@ -56,6 +56,21 @@
 # 
 ## @lc code=start
 using LeetCode
-
-## add your code here:
+function lucky_numbers(matrix::Vector{Vector{Int}})::Vector{Int}
+    m, n = length(matrix), length(matrix[1])
+    row_min = Dict{Int,Tuple{Int,Int}}(i => (matrix[i][1], 1) for i in 1:m)
+    col_max = Dict{Int,Tuple{Int,Int}}(j => (0, 0) for j in 1:n)
+    for i in 1:m
+        for j in 1:n
+            c = matrix[i][j]
+            if c < row_min[i][1]
+                row_min[i] = (c, j)
+            end
+            if c > col_max[j][1]
+                col_max[j] = (c, i)
+            end
+        end
+    end
+    return [row_min[i][1] for i in 1:m if col_max[row_min[i][2]][2] == i]
+end
 ## @lc code=end
