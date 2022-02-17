2dcb947d7c615e036542ea19903bec8cdf3e466d
rex <1073853456@qq.com>
2022-02-17 10:34:04 +0800

    solution to 34

--- src/problems/34.find-first-and-last-position-of-element-in-sorted-array.jl
+++ src/problems/34.find-first-and-last-position-of-element-in-sorted-array.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 34. Find First and Last Position of Element in Sorted Array
 # id: problem34
-# author: Tian Jun
-# date: 2020-10-31
+# author: zhwang
+# date: 2022-02-13
 # difficulty: Medium
 # categories: Array, Binary Search
 # link: <https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/>
@@ -56,5 +56,22 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function search_range(nums::Vector{Int}, target::Int)::Vector{Int}
+    function get_left_border(target)
+        left, right = 1, n
+        while left <= right
+            mid = (left + right) >> 1
+            if nums[mid] >= target
+                right = mid - 1
+            else
+                left = mid + 1
+            end
+        end
+        return left
+    end
+    n = length(nums)
+    left_border = get_left_border(target)
+    (left_border > n || nums[left_border] != target) && return [-1, -1]
+    return [left_border - 1, get_left_border(target + 1) - 2]
+end
 ## @lc code=end
