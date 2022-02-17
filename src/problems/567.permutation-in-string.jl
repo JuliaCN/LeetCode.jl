5da1a41601dd86b05249e18ce60fb23a58a6b391
rex <1073853456@qq.com>
2022-02-17 12:02:24 +0800

    solution to 567

--- src/problems/567.permutation-in-string.jl
+++ src/problems/567.permutation-in-string.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 567. Permutation in String
 # id: problem567
-# author: Tian Jun
-# date: 2020-10-31
+# author: zhwang
+# date: 2022-02-17
 # difficulty: Medium
 # categories: Two Pointers, Sliding Window
 # link: <https://leetcode.com/problems/permutation-in-string/description/>
@@ -43,5 +43,29 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+## Double Pointers
+function check_inclusion(s1::String, s2::String)::Bool
+    empty!(d::Dict{Char,Int}) = for i in 'a':'z' d[i] = 0 end;
+    count1, count2 = Dict{Char,Int}(), Dict{Char,Int}(i=>0 for i in 'a':'z')
+    start, left = true, 1
+    for i in s1
+        count1[i] = haskey(count1, i) ? count1[i] + 1 : 1
+    end
+    for (i, c) in enumerate(s2)
+        if !haskey(count1, c)
+            start = true
+            empty!(count2)
+        elseif count2[c] >= count1[c]
+            for j in left:i
+                s2[j] == c && (left = j + 1;break)
+                count2[s2[j]] -= 1
+            end
+        else
+            start && (start = false; left = i)
+            count2[c] += 1
+            all(count1[key] == count2[key] for key in keys(count1)) && return true
+        end
+    end
+    return false
+end
 ## @lc code=end
