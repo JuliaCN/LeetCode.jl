4a0db57e5f2476e9275bfb3f2ef6f1dcddebadc9
rex <1073853456@qq.com>
2022-03-01 21:53:48 +0800

    simplify solution of 136

--- src/problems/136.single-number.jl
+++ src/problems/136.single-number.jl
@@ -53,11 +53,5 @@
 ## @lc code=start
 using LeetCode
 
-function single_number_136(nums::Vector{Int})::Int
-    a = 0
-    for i in nums
-        a ⊻= i
-    end
-    return a
-end
+single_number_136(nums::Vector{Int}) = reduce(⊻, nums)
 ## @lc code=end
