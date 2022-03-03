7c5a5cd38769bcb1c2c7b614d8d6c39490c14ca0
Yao Su <ysu@probquant.com>
2022-03-03 11:32:56 +0800

    Solution to 48

--- src/problems/48.rotate-image.jl
+++ src/problems/48.rotate-image.jl
@@ -67,5 +67,12 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function rotate_48(matrix::Matrix{Int})
+    n = size(matrix, 1)
+    for i in 1:div(n, 2), j in i:n-i
+        matrix[i, j], matrix[j, n+1-i], matrix[n+1-i, n+1-j], matrix[n+1-j, i] =
+            matrix[n+1-j, i], matrix[i, j], matrix[j, n+1-i], matrix[n+1-i, n+1-j]
+    end
+    return matrix
+end
 ## @lc code=end
