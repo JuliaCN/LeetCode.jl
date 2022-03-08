137f1bf9d9e1fb633acb2dcd652d168abdff0e8a
Indigo <ysu@probquant.com>
2022-03-08 09:51:51 +0800

    Solution to 1091

--- src/problems/1091.shortest-path-in-binary-matrix.jl
+++ src/problems/1091.shortest-path-in-binary-matrix.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 1091. Shortest Path in Binary Matrix
 # id: problem1091
-# author: Tian Jun
-# date: 2020-10-31
+# author: Indigo
+# date: 2022-03-08
 # difficulty: Medium
 # categories: Breadth-first Search
 # link: <https://leetcode.com/problems/shortest-path-in-binary-matrix/description/>
@@ -57,5 +57,29 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function shortest_path_binary_matrix(mat::Matrix{Int})
+    visited = fill(false, size(mat))    
+    q = [CartesianIndex(1, 1)]
+    dist = [1]
+    visited[1, 1] = true
+    directions = [CartesianIndex(i, j) for i in -1:1, j in -1:1 if i != 0 || j != 0]
+    res = 0
+    while !visited[end, end] 
+        frt = popfirst!(q)
+        dst = popfirst!(dist)
+        for dir in directions
+            next_coord = frt + dir
+            next_coord ∉ CartesianIndices(mat) && continue
+            visited[next_coord] && continue
+            visited[next_coord] = true
+            if mat[next_coord] ==0 
+                push!(q, next_coord)
+                push!(dist, dst + 1)
+                res = dst + 1
+            end
+        end
+    end
+    return res
+end
+
 ## @lc code=end
