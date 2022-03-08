f6a4cb23494dcab8fa8440e06a0cd4fe9cfe8e24
Indigo <ysu@probquant.com>
2022-03-08 14:37:28 +0800

    Solution to 1092

--- src/problems/1092.shortest-common-supersequence.jl
+++ src/problems/1092.shortest-common-supersequence.jl
@@ -1,8 +1,8 @@
 # ---
 # title: 1092. Shortest Common Supersequence 
 # id: problem1092
-# author: Tian Jun
-# date: 2020-10-31
+# author: Indigo
+# date: 2022-03-08
 # difficulty: Hard
 # categories: Dynamic Programming
 # link: <https://leetcode.com/problems/shortest-common-supersequence/description/>
@@ -42,5 +42,30 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function shortest_common_supersequence(str1::String, str2::String)
+    m, n = length(str1), length(str2)
+    dp = OffsetArray(fill(0, m + 1, n + 1), -1, -1)
+    for i in 1:m, j in 1:n
+        dp[i, j] = max(
+            dp[i - 1, j], dp[i, j - 1], dp[i - 1, j - 1] + Int(str1[i] == str2[j])
+        )
+    end
+    res = ""
+    coord = CartesianIndex(m, n)
+    while coord != CartesianIndex(0, 0)
+        c1 = coord - CartesianIndex(1, 0)
+        c2 = coord - CartesianIndex(0, 1)
+        if c1 ∈ CartesianIndices(dp) && dp[coord] == dp[c1]
+            res *= str1[coord[1]]
+            coord -= CartesianIndex(1, 0)
+        elseif c2 ∈ CartesianIndices(dp) && dp[coord] == dp[c2]
+            res *= str2[coord[2]]
+            coord -= CartesianIndex(0, 1)
+        else
+            res *= str1[coord[1]]
+            coord -= CartesianIndex(1, 1)
+        end
+    end
+    return reverse(res)
+end
 ## @lc code=end
