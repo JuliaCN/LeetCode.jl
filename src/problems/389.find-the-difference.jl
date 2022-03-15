35f842019731315c5172e3242393c697bb687c62
rex <1073853456@qq.com>
2022-03-15 12:06:26 +0800

    add solution to 389

--- src/problems/389.find-the-difference.jl
+++ src/problems/389.find-the-difference.jl
@@ -69,9 +69,10 @@ function find_the_difference(s::String, t::String)::Char
         cnt[ch - 0x60] += 1
     end
     for ch in codeunits(t)
-        if((cnt[ch - 0x60] -= 1) < 0) 
+        if ((cnt[ch - 0x60] -= 1) < 0)
             return Char(ch)
         end
     end
 end
+find_the_difference_oneline(s::String, t::String) = Char(reduce(⊻, codeunits(s * t)))
 ## @lc code=end
