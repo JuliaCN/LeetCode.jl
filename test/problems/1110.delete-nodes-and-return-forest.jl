d3d2595c3b1db61ccdf50b2bb7515d9aeca1e6bc
Indigo <j.c.f.gauss@sjtu.edu.cn>
2022-03-19 12:31:23 +0800

    Solution to 1110

---
+++ test/problems/1110.delete-nodes-and-return-forest.jl
@@ -0,0 +1,3 @@
+@testset "1110.delete-nodes-and-return-forest.jl" begin
+    @test del_nodes1110(TreeNode{Int}([1, 2, 3, 4, 5, 6, 7]), [3, 5]) == [TreeNode{Int}([1, 2, nothing, 4]), TreeNode(6), TreeNode(7)]
+end
