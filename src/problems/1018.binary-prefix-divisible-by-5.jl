0daadc2ee5d8c0854d367bc3f82f368575bf1d9c
Indigo <j.c.f.gauss@sjtu.edu.cn>
2022-03-19 16:56:43 +0800

    Solution to 1019

--- src/problems/1018.binary-prefix-divisible-by-5.jl
+++ src/problems/1018.binary-prefix-divisible-by-5.jl
@@ -63,12 +63,12 @@ using LeetCode
 
 function prefixes_div_by5(nums::Vector{Int})
     len = length(nums)
-    res = fill(false, len)    
+    res = fill(false, len)
     cur = 0
     for i in 1:len
         cur = cur * 2 + nums[i]
         cur % 5 == 0 && (res[i] = true)
     end
-    res
+    return res
 end
 ## @lc code=end
