aafa25482b7502e91f5bbae9f7e87ac0cc088c7f
rex <1073853456@qq.com>
2022-08-05 01:29:12 +0800

    edit tests of 622

--- test/problems/622.design-circular-queue.jl
+++ test/problems/622.design-circular-queue.jl
@@ -1,16 +1,32 @@
 @testset "622.design-circular-queue.jl" begin
-    function check622(k::Int, funcs::Vector{String}, paras::Vector, reses::Vector)
-        que = MyCircularQueue(k) 
-        all(eval(Meta.parse(func))(que, para...) == res for (func, para, res) in zip(funcs, paras, reses))
-    end
     ## test 1
-    funcs = ["en_queue","en_queue","en_queue","en_queue","rear","is_full","de_queue","en_queue","rear"]
-    paras = [[1],[2],[3],[4],[],[],[],[4],[]]
-    reses = [true,true,true,false,3,true,true,true,4]
-    @test check622(3, funcs, paras, reses)
+    @testset "test1" begin
+        que = MyCircularQueue(3)
+        @test all([
+            en_queue(que, 1) == true,
+            en_queue(que, 2) == true,
+            en_queue(que, 3) == true,
+            en_queue(que, 4) == false,
+            rear(que) == 3,
+            is_full(que) == true,
+            de_queue(que) == true,
+            en_queue(que, 4) == true,
+            rear(que) == 4])
+    end
     
-    funcs = ["en_queue", "rear", "front", "de_queue", "front", "de_queue", "front", "en_queue", "en_queue", "en_queue", "en_queue"]
-    paras = [[2],[],[],[],[],[],[],[4],[2],[2],[3]]
-    reses = [true,2,2,true,-1,false,-1,true,true,true,false]
-    @test check622(3, funcs, paras, reses)
+    # test 2
+    @testset "test2" begin
+        que = MyCircularQueue(3)
+        @test all([en_queue(que, 2) == true,
+        rear(que) == 2, 
+        front(que) == 2,
+        de_queue(que) == true,
+        front(que) == -1,
+        de_queue(que) == false,
+        front(que) == -1,
+        en_queue(que, 4) == true,
+        en_queue(que, 2) == true,
+        en_queue(que, 2) == true,
+        en_queue(que, 3) == false])
+    end
 end
