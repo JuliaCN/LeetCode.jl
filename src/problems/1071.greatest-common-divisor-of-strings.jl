34dd596ce443a49a9a0eda297c7957d509eb8280
Indigo <ysu@smail.nju.edu.cn>
2022-09-29 09:28:41 +0800

    Solution to 1071

--- src/problems/1071.greatest-common-divisor-of-strings.jl
+++ src/problems/1071.greatest-common-divisor-of-strings.jl
@@ -62,6 +62,6 @@
 using LeetCode
 
 function gcd_of_strings(str1::String, str2::String)
-    return str1 * str2 == str2 * str1 ? str1[1:gcd(length(str1), length(str2))] : ""    
+    return str1 * str2 == str2 * str1 ? str1[1:gcd(length(str1), length(str2))] : ""
 end
 ## @lc code=end
