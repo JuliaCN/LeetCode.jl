9b6756237604c39620fcd9ab4744251a80206a1e
Indigo <ysu@smail.nju.edu.cn>
2022-10-06 15:04:31 +0800

    Solution to 1081

--- src/problems/1081.smallest-subsequence-of-distinct-characters.jl
+++ src/problems/1081.smallest-subsequence-of-distinct-characters.jl
@@ -44,5 +44,23 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+function smallest_subsequence(s::String)
+    sascii = codeunits(s)[:] .- Int8('a' - 1)
+    scnt = fill(0, 26)
+    cur_cnt = fill(false, 26)
+    cur = UInt8[]
+    for c in sascii
+        scnt[c] += 1
+    end
+    for c in sascii
+        scnt[c] -= 1
+        cur_cnt[c] && continue
+        while !isempty(cur) && cur[end] > c && scnt[cur[end]] > 0
+            cur_cnt[pop!(cur)] = false
+        end
+        push!(cur, c)
+        cur_cnt[c] = true
+    end
+    return join(cur .+ ('a' - 1))
+end
 ## @lc code=end
