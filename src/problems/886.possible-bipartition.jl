34e1d0b02eda6f939b83339b7dc01c89c4a60a8a
Indigo <ysu@smail.nju.edu.cn>
2022-10-16 14:51:15 +0800

    Solution to 886

--- src/problems/886.possible-bipartition.jl
+++ src/problems/886.possible-bipartition.jl
@@ -63,5 +63,20 @@
 ## @lc code=start
 using LeetCode
 
-## add your code here:
+using DataStructures
+function possible_bipartition(n::Int, dislikes::Vector{Vector{Int}})
+    int_ds = IntDisjointSets(n)
+    graph = [Int[] for _ in 1:n]
+    for (a, b) in dislikes
+        push!(graph[a], b)
+        push!(graph[b], a)
+    end
+    for i in 1:n
+        for j in graph[i]
+            union!(int_ds, graph[i][1], j)
+            DataStructures.in_same_set(int_ds, i, j) && return false
+        end
+    end
+    return true
+end
 ## @lc code=end
---
+++ test/problems/886.possible-bipartition.jl
@@ -0,0 +1,5 @@
+@testset "886.possible-bipartition.jl" begin
+    @test possible_bipartition(4, [[1,2],[1,3],[2,4]])
+    @test !possible_bipartition(3, [[1,2],[1,3],[2,3]])
+    @test !possible_bipartition(5, [[1,2],[2,3],[3,4],[4,5],[1,5]])
+end
