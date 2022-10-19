7dbe45d7c712205944a84ea103ba5d940cc285de
Indigo <ysu@smail.nju.edu.cn>
2022-10-19 13:33:37 +0800

    name conflicts

--- src/problems/769.max-chunks-to-make-sorted.jl
+++ src/problems/769.max-chunks-to-make-sorted.jl
@@ -49,7 +49,7 @@
 ## @lc code=start
 using LeetCode
 
-function max_chunks_to_sorted(arr::Vector{Int})
+function max_chunks_to_sorted_ii(arr::Vector{Int})
     return count(i == num + 1 for (i, num) in enumerate(accumulate(max, arr)))
 end
 ## @lc code=end
