e5daa114fa7d5050fc9c4b012d6c58bdb40a5818
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2024-02-20 08:05:38 +0100

    Handle trivial input better in 14.longest-common-prefix.jl (#172)
    * Improve 14.longest-common-prefix.jl
    
    This change makes the algorithm able to handle trivial cases where there is only one input string or where all input strings are equal
    
    * More tests for 14.longest-common-prefix.jl

--- test/problems/14.longest-common-prefix.jl
+++ test/problems/14.longest-common-prefix.jl
@@ -2,4 +2,7 @@
     @test longest_common_prefix(["flower", "flow", "flight"]) == "fl"
     @test longest_common_prefix(["dog", "racecar", "car"]) == ""
     @test longest_common_prefix(["reflower", "flow", "flight"]) == ""
+    @test longest_common_prefix(["abc"]) == "abc"
+    @test longest_common_prefix(fill("abc", 5)) == "abc"
+    @test longest_common_prefix(fill("", 4)) == ""
 end
