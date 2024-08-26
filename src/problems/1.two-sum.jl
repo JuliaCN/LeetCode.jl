0d9b1876bbf8da15e056b7420201724c18504c14
J S <49557684+svilupp@users.noreply.github.com>
2024-08-26 09:43:59 +0100

    Fixed example for 1-based indexing
    Fixing the provided example for Julia's 1-based indexing

--- src/problems/1.two-sum.jl
+++ src/problems/1.two-sum.jl
@@ -10,7 +10,7 @@
 # ---
 # 
 # Given an array of integers `nums` and an integer `target`, return _indices of
-# the two numbers such that they add up to`target`_.
+# the two numbers so that they add up to `target`_.
 # 
 # You may assume that each input would have **_exactly_ one solution**, and you
 # may not use the _same_ element twice.
@@ -24,8 +24,8 @@
 #     
 #     
 #     Input: nums = [2,7,11,15], target = 9
-#     Output: [0,1]
-#     Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+#     Output: [1, 2]
+#     Output: Because nums[1] + nums[2] == 9, we return [1, 2].
 #     
 # 
 # **Example 2:**
@@ -33,7 +33,7 @@
 #     
 #     
 #     Input: nums = [3,2,4], target = 6
-#     Output: [1,2]
+#     Output: [2, 3]
 #     
 # 
 # **Example 3:**
@@ -41,14 +41,14 @@
 #     
 #     
 #     Input: nums = [3,3], target = 6
-#     Output: [0,1]
+#     Output: [1, 2]
 #     
 # 
 # 
 # 
 # **Constraints:**
 # 
-#   * `2 <= nums.length <= 103`
+#   * `2 <= length(nums) <= 103`
 #   * `-109 <= nums[i] <= 109`
 #   * `-109 <= target <= 109`
 #   * **Only one valid answer exists.**
