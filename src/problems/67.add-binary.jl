5e01a5d2aee4a745123cd5398a27f5e53c5bbefe
QueensleyC <ebyqueensley1@gmail.com>
2024-12-12 18:35:10 +0100

    Solution to 67

--- src/problems/67.add-binary.jl
+++ src/problems/67.add-binary.jl
@@ -41,5 +41,17 @@
 ## @lc code=start
 using LeetCode
 
+function add_binary(a::String, b::String)
+
+    num1 = parse(Int64, a; base=2)
+    num2 = parse(Int64, b; base=2)
+
+    sum = num1 + num2
+
+    return string(sum, base=2)
+
+end
+
+
 ## add your code here:
 ## @lc code=end
